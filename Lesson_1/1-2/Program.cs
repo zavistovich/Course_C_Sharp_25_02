8ad75b6a39fb05ee4f069784eb0e24b8eede7584
serge <szavistovich@gmail.com>
2023-03-08 12:53:39 +0300

    Added Task 1-2

---
+++ Lesson_1/1-2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая будет выдавать 
+// название дня недели по заданному номеру
+
+string a = Console.ReadLine()!; // Можно сравнивать строку без преобразования
+// int a = int.Parse (n);
+
+if(a == "1") Console.WriteLine("It's Sunday");
+else if(a == "2") Console.WriteLine("It's Monday");
+else if(a == "3") Console.WriteLine("It's Tuesday");
+else if(a == "4") Console.WriteLine("It's Wednesday");
+else if(a == "5") Console.WriteLine("It's Thursday");
+else if(a == "6") Console.WriteLine("It's Friday");
+else if(a == "7") Console.WriteLine("It's Saturday");
+else Console.WriteLine("There is no such day in the week");
