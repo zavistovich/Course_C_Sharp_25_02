7f6679fcf462276a78329e962bd8f4338c0940d6
serge <szavistovich@gmail.com>
2023-03-08 17:01:47 +0300

    Added 1_2_HW

--- Lesson_1/HW/1_2_HW/Program.cs
+++ Lesson_1/HW/1_2_HW/Program.cs
@@ -5,4 +5,19 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
-Console.WriteLine("Hello, World!");
+
+string na = Console.ReadLine()!;
+string nb = Console.ReadLine()!;
+string nc = Console.ReadLine()!;
+
+int a = int.Parse(na);
+int b = int.Parse(nb);
+int c = int.Parse(nc);
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
